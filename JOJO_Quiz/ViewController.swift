901b6f90fe8ed7852e9bc77fd5d56aac5fe4aafc
Shelly <shelly980102@gmail.com>
2020-01-28 14:06:45 +0800

    Initial Commit

---
+++ JOJO_Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JOJO_Quiz
+//
+//  Created by 蕭聿莘 on 2020/1/28.
+//  Copyright © 2020 Kazuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
